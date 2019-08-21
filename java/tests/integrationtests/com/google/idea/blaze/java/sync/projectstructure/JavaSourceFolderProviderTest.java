47376fbfd2af08e7a7d439311caeed6791732ce6
Vaidas Pilkauskas <vaidas.pilkauskas@gmail.com>
2019-08-21 16:10:07 +0300

    Revert "Move intellij beta branch to 2019.2"
    This reverts commit d3c84f0f38abafb7c189f77cb592651310745f36.

--- java/tests/integrationtests/com/google/idea/blaze/java/sync/projectstructure/JavaSourceFolderProviderTest.java
+++ java/tests/integrationtests/com/google/idea/blaze/java/sync/projectstructure/JavaSourceFolderProviderTest.java
@@ -27,15 +27,11 @@ import com.google.idea.blaze.java.sync.model.BlazeContentEntry;
 import com.google.idea.blaze.java.sync.model.BlazeJavaImportResult;
 import com.google.idea.blaze.java.sync.model.BlazeJavaSyncData;
 import com.google.idea.blaze.java.sync.model.BlazeSourceDirectory;
-import com.intellij.openapi.Disposable;
 import com.intellij.openapi.roots.ContentEntry;
 import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.roots.SourceFolder;
-import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.vfs.VirtualFile;
 import java.io.File;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -43,18 +39,6 @@ import org.junit.runners.JUnit4;
 /** Tests for {@link JavaSourceFolderProvider} */
 @RunWith(JUnit4.class)
 public class JavaSourceFolderProviderTest extends BlazeIntegrationTestCase {
-
-  private Disposable thisClassDisposable; // disposed prior to calling parent class's @After methods
-
-  @Before
-  public void doSetup() {
-    thisClassDisposable = Disposer.newDisposable();
-  }
-
-  @After
-  public void doTearDown() {
-    Disposer.dispose(thisClassDisposable);
-  }
 
   @Test
   public void testInitializeSourceFolders() {
@@ -193,14 +177,8 @@ public class JavaSourceFolderProviderTest extends BlazeIntegrationTestCase {
   }
 
   private ContentEntry getContentEntry(VirtualFile root) {
-    ContentEntry entry =
-        ModuleRootManager.getInstance(testFixture.getModule())
-            .getModifiableModel()
-            .addContentEntry(root);
-    if (entry instanceof Disposable) {
-      // need to dispose the content entry and child disposables before the TestFixture is disposed
-      Disposer.register(thisClassDisposable, (Disposable) entry);
-    }
-    return entry;
+    return ModuleRootManager.getInstance(testFixture.getModule())
+        .getModifiableModel()
+        .addContentEntry(root);
   }
 }
