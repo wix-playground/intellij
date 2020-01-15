6fc40ac02beb01ab28ae62c84ab5c5ee5e675e33
Aleksandr Radchykov <committed.chinchilla@gmail.com>
2020-01-15 08:45:15 +0200

    Prelude support (#53)

--- base/src/com/google/idea/blaze/base/bazel/BuildSystemProvider.java
+++ base/src/com/google/idea/blaze/base/bazel/BuildSystemProvider.java
@@ -193,6 +193,7 @@ public interface BuildSystemProvider {
     possibleBuildFileNames().forEach(s -> list.add(new ExactFileNameMatcher(s)));
     possibleWorkspaceFileNames().forEach(s -> list.add(new ExactFileNameMatcher(s)));
     possibleFileExtensions().forEach(s -> list.add(new ExtensionFileNameMatcher(s)));
+    list.add(new ExactFileNameMatcher("prelude_bazel"));
     possibleModuleFileNames().forEach(s -> list.add(new ExactFileNameMatcher(s)));
     return list.build();
   }
