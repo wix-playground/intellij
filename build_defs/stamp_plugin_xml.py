55472811c2cbcba693ccc05998c891bd05c299f4
Vaidas Pilkauskas <vaidasp@wix.com>
2021-07-21 14:03:05 +0300

    Support full range major version also for EAP

--- build_defs/stamp_plugin_xml.py
+++ build_defs/stamp_plugin_xml.py
@@ -224,13 +224,7 @@ def main():
     idea_version_element = dom.createElement("idea-version")
     new_elements.append(idea_version_element)
 
-    if is_eap:
-      # IU211.6693.111 >> since_build=211.6693 and until_build=211.6693.*
-      build_version = _strip_build_number(
-          _strip_product_code(api_version))
-    else:
-      # IU211.6693.111 >> since_build=211 and until_build=211.*
-      build_version = _parse_major_version(api_version)
+    build_version = _parse_major_version(_strip_product_code(api_version))
 
     if args.stamp_since_build:
       if build_version in since_build_numbers.keys():
