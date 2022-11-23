373ec9dbe238967b20218878e8ca83be600aa5d4
Ittai Zeidman <ittaiz@gmail.com>
2022-11-23 09:00:53 +0200

    Fast test supports Scala junit Via running bloop and BSP
    Still includes a hack of the env variable of BUILD_TOOL which we need to see how to remove but at least a smaller hack than before
    
    FIX NPE

--- java/src/com/google/idea/blaze/java/run/fastbuild/FastBuildTestClassFinder.java
+++ java/src/com/google/idea/blaze/java/run/fastbuild/FastBuildTestClassFinder.java
@@ -33,6 +33,7 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import javax.annotation.Nullable;
 
 final class FastBuildTestClassFinder {
 
@@ -46,7 +47,7 @@ final class FastBuildTestClassFinder {
     return project.getService(FastBuildTestClassFinder.class);
   }
 
-  String getTestClass(Label label, JavaInfo targetJavaInfo) throws ExecutionException {
+  String getTestClass(Label label, JavaInfo targetJavaInfo, @Nullable String testFilter) throws ExecutionException {
     if (targetJavaInfo.testClass().isPresent()) {
       return targetJavaInfo.testClass().get();
     } else {
@@ -58,15 +59,24 @@ final class FastBuildTestClassFinder {
       BlazeProjectData blazeProjectData =
           BlazeProjectDataManager.getInstance(project).getBlazeProjectData();
 
-      Optional<String> testClass =
+      Optional<String> testClassFromSources =
           determineTestClassFromSources(psiManager, blazeProjectData, label, targetJavaInfo);
-      if (testClass.isPresent()) { // In Java9, we could chain these with Optional.or()
-        return testClass.get();
+      if (testClassFromSources.isPresent()) { // In Java9, we could chain these with Optional.or()
+        return testClassFromSources.get();
       }
-
-      return determineTestClassFromPackage(psiManager, blazeProjectData, label)
-          .orElseThrow(() -> new ExecutionException("Couldn't determine test class."));
+      final Optional<String> testClassFromPackage = determineTestClassFromPackage(psiManager, blazeProjectData, label);
+      if (testClassFromPackage.isPresent()) { // In Java9, we could chain these with Optional.or()
+        return testClassFromPackage.get();
+      }
+      if (testFilter != null && !testFilter.trim().isEmpty()) {
+        return getTestClassFromTestFilter(testFilter);
+      }
+      throw new ExecutionException("Couldn't determine test class.");
     }
+  }
+
+  private String getTestClassFromTestFilter(String testFilter) {
+    return testFilter.substring(0, testFilter.indexOf('#'));
   }
 
   // This is the first part of Bazel's JavaCommon.determinePrimaryClass()
