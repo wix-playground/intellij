7bb6caa953c9e0e57a54f9203f5b91094feeba43
Vaidas Pilkauskas <vaidasp@wix.com>
2023-09-27 09:52:51 +0300

    Revert "Update plugin-building workflow to support remote builds"
    This reverts commit 8df620877669e1b89b15f5ffde3cb421cab2e073.

--- plugin_dev/src/com/google/idea/blaze/plugin/run/BlazeIntellijPluginDeployer.java
+++ plugin_dev/src/com/google/idea/blaze/plugin/run/BlazeIntellijPluginDeployer.java
@@ -15,7 +15,6 @@
  */
 package com.google.idea.blaze.plugin.run;
 
-import static com.google.common.collect.ImmutableList.toImmutableList;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 import com.google.common.collect.ImmutableList;
@@ -23,29 +22,30 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import com.google.common.io.Files;
 import com.google.devtools.intellij.plugin.IntellijPluginTargetDeployInfo.IntellijPluginDeployFile;
 import com.google.devtools.intellij.plugin.IntellijPluginTargetDeployInfo.IntellijPluginDeployInfo;
 import com.google.idea.blaze.base.async.executor.BlazeExecutor;
+import com.google.idea.blaze.base.command.buildresult.BlazeArtifact;
+import com.google.idea.blaze.base.command.buildresult.BuildResultHelper;
 import com.google.idea.blaze.base.command.buildresult.BuildResultHelper.GetArtifactsException;
 import com.google.idea.blaze.base.command.buildresult.OutputArtifact;
-import com.google.idea.blaze.base.sync.aspects.BlazeBuildOutputs;
+import com.google.idea.blaze.base.io.FileOperationProvider;
+import com.google.idea.blaze.base.model.primitives.Label;
 import com.google.idea.common.experiments.BoolExperiment;
 import com.google.protobuf.TextFormat;
 import com.intellij.concurrency.AsyncUtil;
 import com.intellij.execution.ExecutionException;
 import com.intellij.openapi.util.Key;
-import com.intellij.openapi.util.text.StringUtil;
 import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.Future;
 
@@ -59,14 +59,16 @@ class BlazeIntellijPluginDeployer {
       Key.create(BlazeIntellijPluginDeployer.class.getName());
 
   private final String sandboxHome;
-  private final Map<String, OutputArtifact> buildArtifactsMap = new HashMap<>();
-  private final List<OutputArtifact> deployInfoArtifacts = new ArrayList<>();
-  private final Map<OutputArtifact, File> filesToDeploy = Maps.newHashMap();
+  private final Label pluginTarget;
+  private final List<File> deployInfoFiles = new ArrayList<>();
+  private final Map<File, File> filesToDeploy = Maps.newHashMap();
+  private File executionRoot;
 
   private Future<Void> fileCopyingTask;
 
-  BlazeIntellijPluginDeployer(String sandboxHome) {
+  BlazeIntellijPluginDeployer(String sandboxHome, Label pluginTarget) {
     this.sandboxHome = sandboxHome;
+    this.pluginTarget = pluginTarget;
   }
 
   /**
@@ -74,49 +76,49 @@ class BlazeIntellijPluginDeployer {
    * previously built plugin.
    */
   void buildStarted() {
-    deployInfoArtifacts.clear();
-    buildArtifactsMap.clear();
+    executionRoot = null;
+    deployInfoFiles.clear();
   }
 
-  void reportBuildComplete(BlazeBuildOutputs blazeBuildOutputs) throws GetArtifactsException {
-    ImmutableList<OutputArtifact> buildArtifacts =
-        blazeBuildOutputs.artifacts.values().stream()
-            .map(a -> a.artifact)
-            .collect(toImmutableList());
-    buildArtifactsMap.clear();
-    buildArtifacts.forEach(a -> buildArtifactsMap.put(a.getRelativePath(), a));
-
-    deployInfoArtifacts.clear();
-    deployInfoArtifacts.addAll(
-        buildArtifacts.stream()
-            .filter(a -> a.toString().endsWith(".intellij-plugin-debug-target-deploy-info"))
-            .collect(toImmutableList()));
+  void reportBuildComplete(File executionRoot, BuildResultHelper buildResultHelper)
+      throws GetArtifactsException {
+    this.executionRoot = executionRoot;
+    deployInfoFiles.clear();
+    ImmutableList<OutputArtifact> outputs =
+        buildResultHelper.getBuildArtifactsForTarget(
+            pluginTarget, file -> file.endsWith(".intellij-plugin-debug-target-deploy-info"));
+    deployInfoFiles.addAll(BlazeArtifact.getLocalFiles(outputs));
   }
 
   /**
    * Returns information about which plugins will be deployed, and asynchronously copies the
    * corresponding files to the sandbox.
    */
-  DeployedPluginInfo deployNonBlocking(String buildSystem) throws ExecutionException {
-    if (deployInfoArtifacts.isEmpty()) {
+  DeployedPluginInfo deployNonBlocking() throws ExecutionException {
+    if (deployInfoFiles.isEmpty()) {
       throw new ExecutionException("No plugin files found. Did the build fail?");
     }
     List<IntellijPluginDeployInfo> deployInfoList = Lists.newArrayList();
-    for (OutputArtifact deployInfoFile : deployInfoArtifacts) {
+    for (File deployInfoFile : deployInfoFiles) {
       deployInfoList.addAll(readDeployInfoFromFile(deployInfoFile));
     }
-    ImmutableMap<OutputArtifact, File> filesToDeploy =
-        getFilesToDeploy(deployInfoList, buildSystem);
+    ImmutableMap<File, File> filesToDeploy = getFilesToDeploy(executionRoot, deployInfoList);
     this.filesToDeploy.putAll(filesToDeploy);
     ImmutableSet<File> javaAgentJars =
         deployJavaAgents.getValue() ? listJavaAgentFiles(deployInfoList) : ImmutableSet.of();
 
+    for (File file : filesToDeploy.keySet()) {
+      if (!file.exists()) {
+        throw new ExecutionException(
+            String.format("Plugin file '%s' not found. Did the build fail?", file.getName()));
+      }
+    }
     // kick off file copying task asynchronously, so it doesn't block the EDT.
     fileCopyingTask =
         BlazeExecutor.getInstance()
             .submit(
                 () -> {
-                  for (Map.Entry<OutputArtifact, File> entry : filesToDeploy.entrySet()) {
+                  for (Map.Entry<File, File> entry : filesToDeploy.entrySet()) {
                     copyFileToSandbox(entry.getKey(), entry.getValue());
                   }
                   return null;
@@ -139,10 +141,10 @@ class BlazeIntellijPluginDeployer {
     }
   }
 
-  private static ImmutableList<IntellijPluginDeployInfo> readDeployInfoFromFile(
-      OutputArtifact deployInfoArtifact) throws ExecutionException {
+  private static ImmutableList<IntellijPluginDeployInfo> readDeployInfoFromFile(File deployInfoFile)
+      throws ExecutionException {
     ImmutableList.Builder<IntellijPluginDeployInfo> result = ImmutableList.builder();
-    try (InputStream inputStream = deployInfoArtifact.getInputStream()) {
+    try (InputStream inputStream = new BufferedInputStream(new FileInputStream(deployInfoFile))) {
       IntellijPluginDeployInfo.Builder builder = IntellijPluginDeployInfo.newBuilder();
       TextFormat.Parser parser = TextFormat.Parser.newBuilder().setAllowUnknownFields(true).build();
       parser.merge(new InputStreamReader(inputStream, UTF_8), builder);
@@ -154,43 +156,22 @@ class BlazeIntellijPluginDeployer {
     return result.build();
   }
 
-  private ImmutableMap<OutputArtifact, File> getFilesToDeploy(
-      Collection<IntellijPluginDeployInfo> deployInfos, String buildSystem)
-      throws ExecutionException {
-    ImmutableMap.Builder<OutputArtifact, File> result = ImmutableMap.builder();
+  private ImmutableMap<File, File> getFilesToDeploy(
+      File executionRoot, Collection<IntellijPluginDeployInfo> deployInfos) {
+    ImmutableMap.Builder<File, File> result = ImmutableMap.builder();
     for (IntellijPluginDeployInfo deployInfo : deployInfos) {
       for (IntellijPluginDeployFile deployFile : deployInfo.getDeployFilesList()) {
-        result.put(
-            getArtifactFromDeployFile(deployFile, buildSystem),
-            new File(sandboxPluginDirectory(sandboxHome), deployFile.getDeployLocation()));
+        File src = new File(executionRoot, deployFile.getExecutionPath());
+        File dest = new File(sandboxPluginDirectory(sandboxHome), deployFile.getDeployLocation());
+        result.put(src, dest);
       }
       for (IntellijPluginDeployFile deployFile : deployInfo.getJavaAgentDeployFilesList()) {
-        result.put(
-            getArtifactFromDeployFile(deployFile, buildSystem),
-            new File(sandboxPluginDirectory(sandboxHome), deployFile.getDeployLocation()));
+        File src = new File(executionRoot, deployFile.getExecutionPath());
+        File dest = new File(sandboxPluginDirectory(sandboxHome), deployFile.getDeployLocation());
+        result.put(src, dest);
       }
     }
     return result.build();
-  }
-
-  private OutputArtifact getArtifactFromDeployFile(
-      IntellijPluginDeployFile deployFile, String buildSystem) throws ExecutionException {
-    String relativePath =
-        buildArtifactsMap.keySet().stream()
-            .filter(
-                key ->
-                    key.endsWith(
-                        StringUtil.trimStart(
-                            deployFile.getExecutionPath(),
-                            String.format("%s-out/", buildSystem.toLowerCase(Locale.ROOT)))))
-            .findAny()
-            .orElseThrow(
-                () ->
-                    new ExecutionException(
-                        String.format(
-                            "Plugin file '%s' not found. Did the build fail?",
-                            deployFile.getExecutionPath())));
-    return buildArtifactsMap.get(relativePath);
   }
 
   private ImmutableSet<File> listJavaAgentFiles(Collection<IntellijPluginDeployInfo> deployInfos) {
@@ -207,12 +188,10 @@ class BlazeIntellijPluginDeployer {
     return new File(sandboxHome, "plugins");
   }
 
-  private static void copyFileToSandbox(OutputArtifact deployArtifact, File dest)
-      throws ExecutionException {
-    try (BufferedInputStream stream = deployArtifact.getInputStream()) {
-      boolean unused = dest.getParentFile().mkdirs();
-      Files.write(stream.readAllBytes(), dest);
-      unused = dest.setExecutable(true, true);
+  private static void copyFileToSandbox(File src, File dest) throws ExecutionException {
+    try {
+      dest.getParentFile().mkdirs();
+      FileOperationProvider.getInstance().copy(src, dest, StandardCopyOption.REPLACE_EXISTING);
       dest.deleteOnExit();
     } catch (IOException e) {
       throw new ExecutionException("Error copying plugin file to sandbox", e);
